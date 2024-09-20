098e08bb6e9e06fc8fde70347eb79239ce915c0b
hossamghoneim <hossamghoneim11@gmail.com>
2024-09-20 16:51:47 +0300

    updates

--- resources/demo-1/views/partials/dashboard/head.blade.php
+++ resources/demo-1/views/partials/dashboard/head.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; frame-src https://www.google.com/;">
     <link rel="shortcut icon" href="{{asset('placeholder_images/logo.svg')}}" />
 
     <!--begin::Fonts-->
